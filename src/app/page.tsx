d79707375ff7e0765c2a2e00f098917825ea005f
Jenny Huang <jennyyyhuang@yahoo.de>
2026-04-15 00:04:32 +0200

    remove unused assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen flex-col items-center justify-center p-8">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <h1 className="text-4xl font-bold">Unity in the Community</h1>
         <p className="mt-4 text-lg">Welcome to your Next.js app!</p>
       </main>
