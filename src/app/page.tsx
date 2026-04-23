9186c627b18b454b15d4d9404126b67b58a2d6ea
Jenny Huang <jennyyyhuang@yahoo.de>
2026-04-24 01:31:05 +0200

    move home page text to bottom right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { Faker, en } from '@faker-js/faker';
+
+const faker = new Faker({ locale: [en] });
+faker.seed(303);
+
+const description = faker.lorem.paragraph();
+
 export default function Home() {
   return (
-    <>
+    <section className="flex h-full flex-col items-end justify-end text-right">
       <h1>Unity in the Community</h1>
-      <p>Welcome to the project homepage.</p>
-    </>
+      <p className="max-w-xl">{description}</p>
+    </section>
   );
 }
